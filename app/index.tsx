5cad028988d8bfd701de76eb0697024c2aa015c5
krasnoff <krasnoff.kobi@gmail.com>
2025-03-24 15:38:31 +0200

    now fetch data headlines

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,23 @@
 import { Text, View, StyleSheet } from 'react-native';
  import { Link } from 'expo-router'; 
+import useGetData from '../hooks/useGetData';
+import { useEffect } from 'react';
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About screen
-      </Link>
-    </View>
-  );
+    const { data, error, loading, refetch } = useGetData('top-headlines?language=en');
+
+    useEffect(() => {
+        refetch();
+    }, []);
+
+    return (
+        <View style={styles.container}>
+        <Text style={styles.text}>Home screen</Text>
+        <Link href="/about" style={styles.button}>
+            Go to About screen
+        </Link>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
---
+++ hooks/useGetData.ts
@@ -0,0 +1,35 @@
+import { useCallback, useState } from "react";
+import { Methods } from "../enums/methods.enum";
+import axios from "axios";
+
+const useGetData = (url: string, method: Methods = Methods.GET, body: object = {}) => {
+    const [data, setData] = useState<any>(null);
+    const [error, setError] = useState<any>(null);
+    const [loading, setLoading] = useState<boolean>(false);
+
+    const fetchData = useCallback(async () => {
+        setLoading(true);
+        setError(null);
+        try {
+            const fullUrl = process.env.EXPO_PUBLIC_BASE_URL + url
+            
+            const response = await axios({
+                method: method,
+                url: fullUrl,
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Api-Key": process.env.EXPO_PUBLIC_API_KEY
+                },
+            });
+            setData(response.data);
+        } catch (err) {
+            setError(err);
+        } finally {
+            setLoading(false);
+        }
+    }, [url, method, body]);
+
+    return { data, error, loading, refetch: fetchData };
+}
+
+export default useGetData;
