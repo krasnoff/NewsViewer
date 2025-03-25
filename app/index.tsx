deec68ced70a74552af5dc0b047c7091bb0fae03
krasnoff <krasnoff.kobi@gmail.com>
2025-03-25 14:24:14 +0200

    ux design

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View, StyleSheet, Button } from 'react-native';
+import { Text, View, StyleSheet, Button, ScrollView } from 'react-native';
  import { Link } from 'expo-router'; 
 import useGetData from '../hooks/useGetData';
 import { useEffect } from 'react';
@@ -30,9 +30,11 @@ export default function Index() {
                 Go to About screen
             </Link>
             <Button onPress={refetch} title="Press" color="#fff" />
+            <ScrollView>
             {data && data.articles && data.articles.map((item: Articles, index: number) => 
-                <div key={index} style={styles.text}>{item.author}</div>
+                <div key={index} style={styles.itemContainer}>{item.author}</div>
             )}
+            </ScrollView>
         </View>
     );
 }
@@ -40,16 +42,33 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#25292e',
+    backgroundColor: '#FFFFFF',
     alignItems: 'center',
     justifyContent: 'center',
+    color: '#000000',
+    width: '100%'
+  },
+  itemContainer: {
+    flexDirection: 'column',
+    backgroundColor: '#fff',
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    width: '100%',
+    flex: 1
   },
   text: {
-    color: '#fff',
+    color: '#000000',
   },
   button: {
     fontSize: 20,
     textDecorationLine: 'underline',
-    color: '#fff',
+    color: '#000000',
   },
 });
