31c367ae698b3051bddf9b20550ce39a432fe702
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-03-25 22:27:13 +0200

    minor fix

--- hooks/useGetData.ts
+++ hooks/useGetData.ts
@@ -1,7 +1,7 @@
 import { useCallback, useState } from "react";
 import { Methods } from "../enums/methods.enum";
 import axios from "axios";
-import { Articles, NewsData } from "../interfaces/NewsData";
+import { NewsData } from "../interfaces/NewsData";
 
 const useGetData = (url: string, method: Methods = Methods.GET, body: object = {}) => {
     const [data, setData] = useState<NewsData>({ status: '', totalResults: 0, articles: [] });
