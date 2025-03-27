8b32536257aa8c7a06eb11ee04859aa016b97b77
krasnoff <krasnoff.kobi@gmail.com>
2025-03-27 14:41:54 +0200

    header design

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,7 +2,16 @@ import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack screenOptions={{
+        headerStyle: {
+          backgroundColor: '#f4511e',
+        },
+        headerTintColor: '#fff',
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+    }}>
       <Stack.Screen name="index" options={{ title: 'Home' }} />
       <Stack.Screen name="article" options={{ title: 'Article' }} />
     </Stack>
