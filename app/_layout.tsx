f674bae624b847cf23425fa8492fc8f0cff66531
Kobi Krasnoff <krasnoff.kobi@gmail.com>
2025-03-29 11:14:10 +0300

    fix direction problem

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,10 @@
 import { Stack } from 'expo-router';
+import { I18nManager } from 'react-native';
 
 export default function RootLayout() {
+  I18nManager.allowRTL(false);
+  I18nManager.forceRTL(false);
+  
   return (
     <Stack screenOptions={{
         headerStyle: {
